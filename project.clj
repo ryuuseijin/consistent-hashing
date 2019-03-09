941450edd7d6e839ca60482b4432b519397202ea
ryuuseijin <ryuuseijin@example.com>
2019-03-09 16:00:50 +1100

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ryuuseijin/consistent-hashing "0.1.0-SNAPSHOT"
+(defproject ryuuseijin/consistent-hashing "0.1.0"
   :description "A consistent hashing implementation."
   :url "https://github.com/ryuuseijin/consistent-hashing"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
