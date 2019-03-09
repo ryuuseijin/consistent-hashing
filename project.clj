d76e24c516fc49dcdc4c3006c3ee1af8cf4b19e7
ryuuseijin <ryuuseijin@example.com>
2019-03-09 15:20:58 +1100

    chore: add clojars repo

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/ryuuseijin/simple-consistent-hashing"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :repositories [["releases" {:url "https://repo.clojars.org"}]]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [digest "1.4.8"]]
   :repl-options {:init-ns simple-consistent-hashing.core})
